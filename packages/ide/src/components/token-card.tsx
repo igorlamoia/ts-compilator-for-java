e394b4a6733fb080200231a8fe04418376860a78
igorlamoia_hbi <igor@hbifinanceira.com>
2024-10-27 11:22:58 -0300

    style: individual card

--- packages/ide/src/components/token-card.tsx
+++ packages/ide/src/components/token-card.tsx
@@ -27,26 +27,38 @@ export function TokenCard({ token, styles }: TokenCardProps) {
       className={`
         ${styles.bg}
         bg-opacity-80
-
+        relative
         w-64 shadow-sm  rounded-sm p-4
         cursor-pointer
         ${styles.border} text-${styles.text} ${styles.transform}`}
       onClick={handleTokenClick}
     >
-      <p className="text-slate-600">
-        <strong>Line: </strong> <span>{token.line}</span>
-      </p>
-      <p className="text-slate-600">
-        <strong>Column: </strong> <span>{token.column}</span>
-      </p>
-      <p className="text-slate-600">
+      <div className="text-slate-600 grid grid-cols-12 text-sm">
+        <div className="col-span-5">
+          <strong>Line: </strong> <span>{token.line}</span>
+        </div>
+        <div
+          // absolute top-[-10] left-[108]
+
+          className={`
+            col-span-2
+            text-center
+          rounded-md ${styles.bg} bg-opacity-80 py-0 px-2.5 border border-transparent text-sm text-slate-600 transition-all shadow-sm`}
+        >
+          <span className={styles.text}>
+            <strong>{token.type}</strong>
+          </span>
+        </div>
+        <div className="col-span-5 text-end">
+          <strong>Column: </strong> <span>{token.column}</span>
+        </div>
+      </div>
+      <div
+        className={`ml-auto rounded-md ${styles.bg} mt-2 bg-opacity-30 py-1 px-2.5 border border-transparent text-sm text-slate-600 transition-all shadow-sm`}
+      >
         <strong>Lexeme: </strong>
         <span className={styles.text}>{token.lexeme}</span>
-      </p>
-      <p className="text-slate-600">
-        <strong>Type: </strong>
-        <span className={styles.text}>{token.type}</span>
-      </p>
+      </div>
     </div>
   );
 }
