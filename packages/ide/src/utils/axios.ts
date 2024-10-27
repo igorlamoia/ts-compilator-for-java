5bfd63ebfebd7c1b302bd1b0df485aaf2ad4081e
igorlamoia_hbi <igor@hbifinanceira.com>
2024-10-27 12:36:53 -0300

    feat: vercel url

--- packages/ide/src/utils/axios.ts
+++ packages/ide/src/utils/axios.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: process.env.VERCEL_URL
-    ? process.env.VERCEL_URL + "/api"
-    : process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: process.env.VERCEL_URL + "/api",
 });
