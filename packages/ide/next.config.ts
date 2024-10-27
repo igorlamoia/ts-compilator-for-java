61c5de82acc2342bc4e917b1c516298db677af8e
igorlamoia_hbi <igor@hbifinanceira.com>
2024-10-27 16:26:13 -0300

    feat: one package depends on another

--- packages/ide/next.config.ts
+++ packages/ide/next.config.ts
@@ -1,22 +1,5 @@
-const withTM = require("next-transpile-modules")([
-  "@ts-compilator-for-java/compiler",
-]);
-
-module.exports = withTM({
-  webpack: (config, { isServer }) => {
-    // Ensure that webpack processes the compiler package
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.(ts|tsx)$/,
-        include: [require("path").resolve(__dirname, "../compiler")],
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["next/babel", "@babel/preset-typescript"],
-          },
-        },
-      });
-    }
-    return config;
-  },
+import withTM from "next-transpile-modules";
+const withTMInstance = withTM(["@ts-compilator-for-java/compiler"]);
+module.exports = withTMInstance({
+  reactStrictMode: true,
 });
