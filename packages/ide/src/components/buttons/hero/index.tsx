52e3e1a7b86fb0c68b6e4ad40a4f4f3ff06d1650
igorlamoia <igorlamoia@hotmail.com>
2026-02-12 15:40:04 -0300

    Update HeroButton styles and improve button accessibility

--- packages/ide/src/components/buttons/hero/index.tsx
+++ packages/ide/src/components/buttons/hero/index.tsx
@@ -12,8 +12,15 @@ export function HeroButton({
   ...rest
 }: HeroButtonProps) {
   return (
-    <div className={`${styles.buttons} w-max text-white  bg-black `}>
-      <button {...rest} className={`w-40 h-12 ${className} ${styles.btn}`}>
+    <div
+      className={`${styles.buttons} w-max
+          border dark:border-slate-600 border-gray-300
+          dark:bg-slate-800 bg-white
+          dark:text-gray-200 text-gray-700
+          hover:dark:bg-slate-700 hover:bg-gray-50
+          transition-colors text-sm font-medium`}
+    >
+      <button {...rest} className={`w-40 h-9 ${className} ${styles.btn} `}>
         <span></span>
         <p data-start={start} data-title={start} data-text={end}></p>
       </button>
