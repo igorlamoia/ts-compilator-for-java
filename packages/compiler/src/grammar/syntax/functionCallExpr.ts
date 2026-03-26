2591428890dab51e3744138014cbd7b4c80a4637
igorlamoia <igorlamoia@hotmail.com>
2026-03-25 23:21:05 -0300

    feat(compiler): validate array call arguments structurally

--- packages/compiler/src/grammar/syntax/functionCallExpr.ts
+++ packages/compiler/src/grammar/syntax/functionCallExpr.ts
@@ -1,4 +1,10 @@
-import { ExprResult, TokenIterator, ValueType } from "../../token/TokenIterator";
+import {
+  ExprResult,
+  FunctionParameterDescriptor,
+  SymbolDescriptor,
+  TokenIterator,
+  ValueType,
+} from "../../token/TokenIterator";
 import { TOKENS } from "../../token/constants";
 import { argumentListStmt } from "./argumentListStmt";
 import { Token } from "../../token";
@@ -29,6 +35,9 @@ export function functionCallExpr(
   if (signature) {
     args.forEach((arg, index) => {
       const parameter = signature.params[index];
+      if (parameter) {
+        validateArgumentForParameter(iterator, arg, parameter);
+      }
       const expectedType =
         parameter?.kind === "array" ? parameter.baseType : parameter?.type ?? "unknown";
       iterator.warnIfLossyIntConversion(expectedType, arg.type, arg.token);
@@ -45,4 +54,64 @@ export function functionCallExpr(
   iterator.registerTemp(resultTemp, returnType);
 
   return iterator.createExprResult(resultTemp, returnType, functionName);
+}
+
+function validateArgumentForParameter(
+  iterator: TokenIterator,
+  arg: ExprResult,
+  parameter: FunctionParameterDescriptor,
+): void {
+  const argumentDescriptor = resolveArgumentDescriptor(iterator, arg);
+
+  if (parameter.kind === "scalar") {
+    if (argumentDescriptor.kind === "array") {
+      throwArgumentTypeError(iterator, arg);
+    }
+    return;
+  }
+
+  if (argumentDescriptor.kind !== "array") {
+    throwArgumentTypeError(iterator, arg);
+  }
+
+  if (argumentDescriptor.baseType !== parameter.baseType) {
+    throwArgumentTypeError(iterator, arg);
+  }
+
+  if (argumentDescriptor.dimensions !== parameter.dimensions) {
+    throwArgumentTypeError(iterator, arg);
+  }
+
+  if (parameter.arrayMode === "fixed") {
+    const sizesMatch =
+      argumentDescriptor.sizes.length === parameter.sizes.length &&
+      argumentDescriptor.sizes.every((size, index) => size === parameter.sizes[index]);
+
+    if (!sizesMatch) {
+      throwArgumentTypeError(iterator, arg);
+    }
+  }
+}
+
+function throwArgumentTypeError(iterator: TokenIterator, arg: ExprResult): never {
+  iterator.throwError("grammar.unexpected_type", arg.token.line, arg.token.column, {
+    lexeme: arg.token.lexeme,
+    line: arg.token.line,
+    column: arg.token.column,
+  });
+}
+
+function resolveArgumentDescriptor(
+  iterator: TokenIterator,
+  arg: ExprResult,
+): SymbolDescriptor | { kind: "scalar"; type: ValueType } {
+  const descriptor = iterator.resolveSymbolDescriptor(arg.place);
+  if (descriptor.kind === "array") {
+    return descriptor;
+  }
+
+  return {
+    kind: "scalar",
+    type: arg.type,
+  };
 }
